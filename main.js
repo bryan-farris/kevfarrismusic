1632c9220478e795e6d5eaf589c41bde71d7a34b
Bryan Farris <farris.bryan@gmail.com>
2025-05-19 11:16:26 -0500

    V2 bunch of stuff

---
+++ main.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+	
+function loadPartials() {
+  console.log("Running loadPartials");
+
+  const header = document.querySelector('#header-wrap');
+  const footer = document.querySelector('#footer-wrap');
+
+  if (header) {
+	fetch('/header.html')
+	  .then(res => res.text())
+	  .then(data => {
+		header.innerHTML = data;
+		console.log("Header loaded");
+	  });
+  }
+
+  if (footer) {
+	fetch('/footer.html')
+	  .then(res => res.text())
+	  .then(data => {
+		footer.innerHTML = data;
+		console.log("Footer loaded");
+	  });
+  }
+}
+
+// ✅ Load once on first page load
+loadPartials();
+
+
+
+});
